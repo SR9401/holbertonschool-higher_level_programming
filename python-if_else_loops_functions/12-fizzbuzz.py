525ef7e313da2d751f15aca72ea73eb4b9da786e
SR9401 <shakibrojas@icloud.com>
2025-05-07 16:40:33 +0200

    t12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,9 @@ def fizzbuzz():
 
     i = 1
     while i != 101:
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
