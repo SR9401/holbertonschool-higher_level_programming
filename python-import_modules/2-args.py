68a303fb7aefb7eab4d3e245fa4acbf4eaacef82
SR9401 <shakibrojas@icloud.com>
2025-05-09 09:54:45 +0200

    t 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ i = 1
 if count == 0:
     print("{} arguments.".format(count))
 elif count == 1:
-    print("{} argument.".format(count))
+    print("{} argument:".format(count))
     print("{}: {}".format(i, sys.argv[i]))
 else:
     print("{} arguments:".format(count))
