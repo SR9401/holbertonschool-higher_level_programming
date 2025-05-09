69fd81b3ca4d92f971807578462afae2c2ad8b67
SR9401 <shakibrojas@icloud.com>
2025-05-09 09:53:48 +0200

    t 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,12 @@ import sys
 count = len(sys.argv) - 1
 i = 1
 if count == 0:
+    print("{} arguments.".format(count))
+elif count == 1:
     print("{} argument.".format(count))
+    print("{}: {}".format(i, sys.argv[i]))
 else:
-    print("{} argument:".format(count))
+    print("{} arguments:".format(count))
     while i <= count:
         print("{}: {}".format(i, sys.argv[i]))
         i += 1
