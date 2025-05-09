a4da33737ae2c889a74e027661b57f7eef3d134b
SR9401 <shakibrojas@icloud.com>
2025-05-09 10:18:46 +0200

    t 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv)
-i = 1
-res = 0
-while i <= count:
-	y = sys.argv[i]
-	x = int(y)
-	res += x
-	i += 1
-print("{}".format(res))
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    i = 1
+    res = 0
+    while i <= (count - 1):
+        y = int(sys.argv[i])
+        res += y
+        i += 1
+    print("{}".format(res))
