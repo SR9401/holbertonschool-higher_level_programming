663d0bf13e757129db4d7686b955ea453f32ee3f
SR9401 <shakibrojas@icloud.com>
2025-05-12 09:51:55 +0200

    tache 0 tache 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-
-    def print_list_integer(my_list=[]):
-        i = 0
-        count = len(my_list[])
-        while i <= count:
-            print("{}".format(my_list[i]))
-            i += 1
+def print_list_integer(my_list=[]):
+    i = 0
+    count = len(my_list) - 1
+    while i <= count:
+        print("{}".format(my_list[i]))
+        i += 1
