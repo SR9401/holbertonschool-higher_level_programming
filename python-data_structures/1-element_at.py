ea6de0112fa90d8c33587fc3119fa007ef5936d4
SR9401 <shakibrojas@icloud.com>
2025-05-12 10:15:31 +0200

    test tache 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-
+    count = len(my_list) - 1
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > count:
         return None
     else:
         return my_list[idx]
