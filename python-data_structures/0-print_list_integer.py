0ed72e88be44910cadc68ad8cb552727836ac73d
SR9401 <shakibrojas@icloud.com>
2025-05-12 09:55:28 +0200

    tache 0 tache 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,5 +4,5 @@ def print_list_integer(my_list=[]):
     i = 0
     count = len(my_list) - 1
     while i <= count:
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
         i += 1
