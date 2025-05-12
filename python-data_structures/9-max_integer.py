21628e1c89f7cdbc7bea040ce7314746ae4653f9
SR9401 <shakibrojas@icloud.com>
2025-05-12 16:16:24 +0200

    t 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    tri = sorted(my_list)
-    i = len(my_list) - 1
-    val_max = tri[i]
-    return val_max
+    if not my_list:
+        return None
+    else:
+        tri = sorted(my_list)
+        i = len(my_list) - 1
+        val_max = tri[i]
+        return val_max
