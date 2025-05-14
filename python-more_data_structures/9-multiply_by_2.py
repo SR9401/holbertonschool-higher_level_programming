faf3a62acb361a7d11b066cb877af66fb204ccd0
SR9401 <shakibrojas@icloud.com>
2025-05-14 12:08:44 +0200

    t 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import copy
-
 def multiply_by_2(a_dictionary):
-    n_dictionary = copy.deepcopy(a_dictionary)
+    n_dictionary = {}
+    for key in a_dictionary:
+        n_dictionary[key] = a_dictionary[key]
+
     for key in n_dictionary:
         n_dictionary[key] = n_dictionary[key] * 2
     return n_dictionary
