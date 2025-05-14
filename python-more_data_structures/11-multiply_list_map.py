45d4c1fc00b7cf184c0f69ebeb263a87bb80107c
SR9401 <shakibrojas@icloud.com>
2025-05-14 14:07:30 +0200

    t 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    n_list = list.copy(my_list)
-    for i in range(len(n_list)):
-        n_list[i] = n_list[i] * number
+    n_list = list(map(lambda x: x * number, my_list))
     return n_list
