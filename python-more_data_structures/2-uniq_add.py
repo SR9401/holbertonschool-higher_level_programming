f2f790948b96ef0206c67e680187d52d54c4024a
SR9401 <shakibrojas@icloud.com>
2025-05-14 11:29:35 +0200

    correct t2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     verif = []
-    i = 0
     res = 0
-    verif.append(my_list[0])
-    res += my_list[0]
-    while i != len(my_list):  
-        v = 0
-        while v != len(verif):
-            if verif[v] == my_list[i]:
-                i += 1
-            v += 1
-        verif.append(my_list[i])
-        res += my_list[i]
-        i += 1
+    for i in my_list:
+        if i not in verif:
+            verif.append(i)
+            res += i
     return res
