2023afcaff7a88cc9bb7682b8cefad9a42a71fa8
SR9401 <shakibrojas@icloud.com>
2025-05-15 14:50:01 +0200

    push t 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        r = a / b
+        print("Inside result: {}".format(r))
+        return r
+    except ZeroDivisionError:
+        print("Inside result: None")
+        return None
