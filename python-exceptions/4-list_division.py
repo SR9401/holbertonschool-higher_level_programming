8cab36ebd58128b8996e48a975cb9bee56545482
SR9401 <shakibrojas@icloud.com>
2025-05-15 15:15:50 +0200

    push t4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    n_list = []
+    while i != list_length:
+        try:
+            n_list.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            n_list.append(0)
+            print("division by 0")
+        except TypeError:
+            n_list.append(0)
+            print("wrong type")
+        except IndexError :
+            n_list.append(0)
+            print("out of range")
+        finally:
+            i += 1
+    return n_list
+    
