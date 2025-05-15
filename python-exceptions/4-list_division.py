381e58477a9f68a8f5bbdff4ec6ae05452712f97
SR9401 <shakibrojas@icloud.com>
2025-05-15 15:55:25 +0200

    correct pycode

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,10 +11,9 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             n_list.append(0)
             print("wrong type")
-        except IndexError :
+        except IndexError:
             n_list.append(0)
             print("out of range")
         finally:
             i += 1
     return n_list
-    
