c8e85dc217eb143a58877dc7ae210f589d2305ac
SR9401 <shakibrojas@icloud.com>
2025-05-20 12:07:15 +0200

    push task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -16,8 +16,11 @@ class Square:
     def __init__(self, size=0):
         """Initialize the square object with size."""
 
-        self._Square__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        return self.__size * self.__size
