04fbf9fc70bff7a5f42f1d9385b7b75667525e9b
SR9401 <shakibrojas@icloud.com>
2025-05-20 10:11:15 +0200

    push t2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides classes and functions to manage users and greet them.
+It demonstrates basic concepts of object-oriented programming in Python.
+"""
 class Square:
+    '''this class represent a square '''
     def __init__(self, size=0):
         self._Square__size = size
         if not isinstance(size, (int)):
