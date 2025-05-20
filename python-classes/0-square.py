e5cf33cbca0266d7531058b93106063c09ef3801
SR9401 <shakibrojas@icloud.com>
2025-05-20 10:13:39 +0200

    correct comment t 0-1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-
+"""
+This module provides classes and functions to manage users and greet them.
+It demonstrates basic concepts of object-oriented programming in Python.
+"""
 class Square:
+    '''do nothing'''
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module provides classes and functions to manage users and greet them.
+It demonstrates basic concepts of object-oriented programming in Python.
+"""
 class Square:
+    '''define size of square'''
     def __init__(self, size):
         self._Square__size = size
