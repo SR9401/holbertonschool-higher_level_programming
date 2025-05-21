84c472ff850faff29c41a8d5d547720ddba11da5
SR9401 <shakibrojas@icloud.com>
2025-05-21 12:03:23 +0200

    push t6

---
+++ python-classes/6-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class with private size and position attributes.
+It includes methods to calculate the area and print the square using '#'.
+"""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with optional size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square with validation."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with '#' character based on position."""
+        if self.__size == 0:
+            print()
+            return
+
+        for i in range(self.__position[1]):
+            print()
+
+        for j in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
