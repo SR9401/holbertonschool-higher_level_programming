3857034df08da336cd172ab2399d77029a4b9a2e
SR9401 <shakibrojas@icloud.com>
2025-05-21 12:16:32 +0200

    push t 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This module defines a Recatangle class as part of an object-oriented programming
+example. It can be extended later to include attributes and methods relevant
+to rectangle.
+"""
+
 class Rectangle:
     """This class represents a recatangle.
 
