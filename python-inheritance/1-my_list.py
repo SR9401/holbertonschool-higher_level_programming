ee10d99cbb75351e24501051523c4241600497eb
SR9401 <shakibrojas@icloud.com>
2025-05-26 12:14:21 +0200

    push task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""This module defines the MyList class that extends the built-in list
+with a method to print its elements sorted."""
+
 
 class MyList(list):
-    """class mylist inheritance of list"""
+
+    """MyList is a custom list that inherits from the built-in list
+    and adds a method to print its elements in sorted order."""
 
     def print_sorted(self):
-        """sorted list"""
+        """Prints the elements of the list
+        in ascending order without modifying the original list."""
 
         print("{}".format(sorted(self)))
