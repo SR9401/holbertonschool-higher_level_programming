a7f0b2018cfa52fdb33ff80cf3586b606eec8231
SR9401 <shakibrojas@icloud.com>
2025-05-26 11:18:30 +0200

    push t 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""a function that returns the list attributes and methods of an object:"""
+
 
 def lookup(obj):
+
+    """a function that returns the list attributes and methods of an object:"""
+
     return list(dir(obj))
