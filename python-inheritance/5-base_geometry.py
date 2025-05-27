affc73ba22e6d79729b68c97a8218bb1a20dcac9
SR9401 <shakibrojas@icloud.com>
2025-05-27 13:34:52 +0200

    push t5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the MyList class that extends the built-in list
+with a method to print its elements sorted."""
+
+
+class BaseGeometry:
+
+    """an empty class"""
+
+    pass
