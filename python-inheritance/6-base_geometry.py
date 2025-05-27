a8b4b5521c5c74a6cbd90e085486715110e82a9c
SR9401 <shakibrojas@icloud.com>
2025-05-27 14:36:40 +0200

    push t6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""This module defines the MyList class that extends the built-in list
-with a method to print its elements sorted."""
+"""This module defines the BaseGeometry class
+with a placeholder method for area computation."""
 
 
 class BaseGeometry:
 
-    """class Base Geometry"""
+    """BaseGeometry is a base class for geometric shapes.
+    It includes a method placeholder for area computation."""
 
     def area(self):
-        """Public instance method that raises an Exception
-        with the message area() is not implemented"""
+        """Raises an Exception to indicate
+        that the area method is not yet implemented."""
         raise Exception("area() is not implemented")
