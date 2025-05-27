d8563b49289ef6776df7d6fe7f2df4db8c03bab7
SR9401 <shakibrojas@icloud.com>
2025-05-27 11:21:58 +0200

    push t2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the MyList class that extends the built-in list
+with a method to print its elements sorted."""
+
+
+def is_same_class(obj, a_class):
+
+    """ function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False"""
+    return (type(obj) is a_class)
