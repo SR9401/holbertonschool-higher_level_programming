ab8cf86971804cea6ca28d4da66fa9707ebbdb21
SR9401 <shakibrojas@icloud.com>
2025-05-27 14:32:45 +0200

    push t6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines the MyList class that extends the built-in list
+with a method to print its elements sorted."""
+
+
+class BaseGeometry:
+
+    """class Base Geometry"""
+
+    def area(self):
+        """Public instance method that raises an Exception
+        with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
