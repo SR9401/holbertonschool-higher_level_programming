3a38119b5b43db32bad4e0aacd035016c886ffe7
SR9401 <shakibrojas@icloud.com>
2025-05-28 12:07:36 +0200

    push t 10.2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -49,15 +49,11 @@ class Square(Rectangle):
     def __init__(self, size):
         """Instantiation with size"""
         self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        """area of square == size by size"""
-
-        return (self.__size * self.__size)
+        super().__init__(size, size)
 
     def __str__(self):
         """str() should return, the following square description:
         [Rectangle] <size>/<size>"""
 
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return "[Rectangle] {}/{}".format(self._Rectangle__width,
+                                          self._Rectangle__height)
