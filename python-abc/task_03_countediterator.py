71b6c22728aa00c005b49b91f36f7ddc2d848da3
SR9401 <shakibrojas@icloud.com>
2025-05-30 10:46:47 +0200

    correct t 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,16 +2,15 @@
 
 class CountedIterator:
 
-	def __init__(self, iterable):
-		self.iterator = iter(iterable)
-		self.count = 0
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
 
-	def get_count(self):
+    def get_count(self):
 
-		return self.count
-	
-	def __next__(self):
-		item = next(self.iterator)
-		self.count += 1
-		return item
-	
+        return self.count
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
