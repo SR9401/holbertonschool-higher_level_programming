ba490c51e8c871b7f780308e09a0aaa70bcff5e9
SR9401 <shakibrojas@icloud.com>
2025-06-02 09:43:30 +0200

    push t0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Définit une fonction qui lit le contenu d’un fichier texte"""
+
 
 def read_file(filename=""):
-    
+    """Ouvre le fichier en mode lecture ("r"), automatiquement fermé après le bloc"""
+
     with open(filename, "r") as file:
         contents = file.read()
         print(contents, end="")
