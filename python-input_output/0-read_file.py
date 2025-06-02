5e5b189e47d2c7fe7b84249db16c0a01dce57c34
SR9401 <shakibrojas@icloud.com>
2025-06-02 09:40:02 +0200

    push t0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-	
-	with open(filename, "r") as file:
-		contents = file.read()
-		print(contents)
+    
+    with open(filename, "r") as file:
+        contents = file.read()
+        print(contents, end="")
