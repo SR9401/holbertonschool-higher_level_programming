95f6da0fa75d295e08ce466065ce4acc7faf0923
SR9401 <shakibrojas@icloud.com>
2025-06-03 13:55:46 +0200

    correct : add coments to t7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
+"""Script qui ajoute tous les arguments à une liste Python,
+puis les sauvegarde dans un fichier JSON.
+"""
+
 import sys
+
+# Importation des fonctions demandées depuis les modules 5 et 6
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+# Nom du fichier où la liste sera sauvegardée
 filename = "add_item.json"
+
+# Récupération des arguments passés en ligne de commande (sans le nom du script)
 args = sys.argv[1:]
 
+# Tentative de chargement du contenu existant dans le fichier
 try:
-    exist = load_from_json_file(filename)
+    existing_data = load_from_json_file(filename)
 except FileNotFoundError:
-    exist = []
+    # Si le fichier n'existe pas encore, on commence avec une liste vide
+    existing_data = []
 
-exist.extend(args)
-save_to_json_file(exist, filename)
+# Ajout des nouveaux arguments à la liste existante
+existing_data.extend(args)
+
+# Sauvegarde de la liste complète dans le fichier JSON
+save_to_json_file(existing_data, filename)
