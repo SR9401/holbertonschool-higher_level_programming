e53e24746d0838213ffa6170585a958029627109
SR9401 <shakibrojas@icloud.com>
2025-06-06 13:52:31 +0200

    corret pycodestyle task 00

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """basic serialization module that adds the functionality
 to serialize a Python dictionary to a JSON file and
 deserialize the JSON file to recreate the Python Dictionary"""
 
-import pickle
+import json
 
 def serialize_and_save_to_file(data, filename):
     """to serialize
     and save data to the specified file"""
 
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """to load and deserialize data
     from the specified file"""
 
-    with open(filename, "rb") as file:
-        return pickle.load(file)
+    with open(filename, "r") as file:
+        return json.load(file)
