6ec9e4a44cdbc4155b6481f2af2548d408f5743c
SR9401 <shakibrojas@icloud.com>
2025-06-06 13:42:51 +0200

    push task 00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    
+    with open(filename, "wb") as file:
+        pickle.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    with open(filename, "rb") as file:
+        return pickle.load(file)
