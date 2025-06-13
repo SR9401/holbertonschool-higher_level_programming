6644ddfc0e57d62d274c2fc3c5f27f5fa4655634
SR9401 <shakibrojas@icloud.com>
2025-06-13 09:27:44 +0200

    push task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,13 +2,14 @@
 """Develop a Simple API using Python with Flask"""
 
 from flask import Flask, jsonify
-import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    """to handle this route"""
+
     return "Welcome to the Flask API!"
 
 
@@ -17,17 +18,22 @@ users = {}
 
 @app.route("/data")
 def get_user():
+    """return a list of all the usernames stored in the API."""
+
     username = list(users.keys())
     return jsonify(username)
 
 
 @app.route("/status")
 def get_status():
+    """ It should return OK"""
+
     return "OK"
 
 
 @app.route("/users/<username>")
 def get_username(username):
+    """ Returns the full object corresponding to the provided username"""
     if username in users:
         return jsonify(users[username])
     else:
