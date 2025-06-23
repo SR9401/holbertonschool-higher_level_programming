5df6f53d07fb89f6b2199240ef01b846eba65cb2
SR9401 <shakibrojas@icloud.com>
2025-06-23 17:19:07 +0200

    push task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -2,4 +2,5 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT score, name 
 FROM second_table
-WHERE LENGTH(name) > 0
+WHERE LENGTH(name) > 0 ANd name IS NOT NULL
+ORDER BY score DESC
