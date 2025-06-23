528f60b17888f9aeef30f5c93e62dc6425f94435
SR9401 <shakibrojas@icloud.com>
2025-06-23 11:19:05 +0200

    push t 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- 5. Full description
+-- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+
+SHOW CREATE TABLE first_table;
