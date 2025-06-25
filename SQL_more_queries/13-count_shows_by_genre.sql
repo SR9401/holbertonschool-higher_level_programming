9c5b78ee3eaea1d276b74037486b0369aab8457c
SR9401 <shakibrojas@icloud.com>
2025-06-25 14:28:20 +0200

    push task 14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,6 @@
 SELECT name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
