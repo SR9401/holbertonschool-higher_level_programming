b4b0c0acfc36342d35d09d21806171b839a5fb49
SR9401 <shakibrojas@icloud.com>
2025-06-26 16:52:51 +0200

    push task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays all values in
+the `states` table of `hbtn_0e_0_usa` where name matches the argument.
 
+It uses MySQLdb to connect to a MySQL server and perform a SELECT query.
+Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
 """
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def list_states_with_name():
     """
-    connects at database
+    Connects to the MySQL database and fetches all rows from the
+    `states` table where the name matches the argument passed.
+    Results are sorted in ascending order by states.id.
     """
     usr = sys.argv[1]
     password = sys.argv[2]
@@ -24,12 +29,14 @@ if __name__ == "__main__":
         db=db_name
     )
     cur = db.cursor()
-
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    list_states_with_name()
