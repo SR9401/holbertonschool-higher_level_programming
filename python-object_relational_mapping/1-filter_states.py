6938803b9c1954b7040e888052ee9013919f6a76
SR9401 <shakibrojas@icloud.com>
2025-06-26 16:21:50 +0200

    push task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+Write a script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa:
 
 """
 import MySQLdb
@@ -18,7 +19,8 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states"
+                "WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
