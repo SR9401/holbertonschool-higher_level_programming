66687e254f2f4d201d311ee039cdc3d85ab34a0c
SR9401 <shakibrojas@icloud.com>
2025-06-27 21:38:35 +0200

    push task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-buuu
+sssss
 """
 
-from model_state import State
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    """"baaaaa"""
+    """
+    ssss
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
@@ -19,12 +21,10 @@ if __name__ == "__main__":
                                                          password, db_name),
         pool_pre_ping=True
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
+    state = session.query(State).order_by(State.id).first()
+    if state:
         print("{}: {}".format(state.id, state.name))
-    session.close()
+    else:
+        print("Nothing")
