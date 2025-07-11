ed20fcfcfaf9b2a47a986c569faf39b6829c14c2
SR9401 <shakibrojas@icloud.com>
2025-07-11 18:51:21 +0200

    push task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const x = parseInt(args[0]);
-let output = '';
+const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x < 0) {
-    console.log("Missing number of occurrences");
-} else {
-    for (let i = 0; i < x; i++) {
-        output += 'C is fun\n';
-    }
-    console.log(output.trim());
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
