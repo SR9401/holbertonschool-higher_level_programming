7ce00868d94a6df81defe7b3266727a4abf4e546
SR9401 <shakibrojas@icloud.com>
2025-07-11 18:54:05 +0200

    push task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-if (args.length === 0 || isNaN(parseInt(args[0]))) {
-    console.log("Missing size");
+const size = parseInt(process.argv[2]);
+
+if (!isNaN(size) && size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 } else {
-    const size = parseInt(args[0]);
-    if (size <= 0) {
-        // Ne rien faire pour les tailles ≤ 0
-    } else {
-        let output = '';
-        for (let i = 0; i < size * size; i++) {
-            output += 'X';
-            if ((i + 1) % size === 0) {
-                output += '\n';
-            }
-        }
-        console.log(output.trim());
-    }
+  console.log('Missing size');
 }
