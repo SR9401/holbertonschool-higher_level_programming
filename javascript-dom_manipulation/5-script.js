ea899f557a453457ee08c474efca628fc3ea3693
SR9401 <shakibrojas@icloud.com>
2025-07-16 10:55:58 +0200

    push task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('update_header').addEventListener('click', function() {
+        const header = document.querySelector('header');
+		header.textContent = 'New Header!!!';
+    });
+});
